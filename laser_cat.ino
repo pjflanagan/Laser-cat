1360e4d3c16cd358fdfdb657ee473b3e7566115e
Peter James Flanagan <pjflanagan1@gmail.com>
2020-02-26 16:23:45 -0500

    Update laser_cat.ino

--- laser_cat.ino
+++ laser_cat.ino
@@ -1,99 +1,146 @@
 /*
-  Laser Tower for the CAT - LA FABRIQUE DIY
-  Pseudo-randomly moves a servo tower (on X and Y axis) and lights up a laser.
-  x_servo is attached to pin 6 and moves in the X plan 
-  y_servo is attached to pin 9 and moves in the Y plan 
-  Laser is on pin 13
-
-  HOW IT WORKS : 
-  The program randomly choose a new position for the laser inside a square you can define below. 
-  It checks the new position is different from the old one of at least "minimal_movement".
-  It moves the tower to the new position and stays still for a time between min_freeze and max_freeze 
-  (this aims to reproduce the behaviour of an insect landing somewhere for a bit and then flying off, 
-  that's the variable you need to increase if your cat is fat).
-  Ans starts the process over and over again. 
-  
-  Created 30 Sep 2016 by Lucas Berbesson
+  Forked from: Laser Tower for the CAT - LA FABRIQUE DIY Created 30 Sep 2016 by Lucas Berbesson
+  TODO:
+    - make it turn on with a button
+    - make it run for 5 minutes before turning off
+    - make it do a few different random things (shake, flicker, flow through points, just go to a selected point)
 */
-
 
 #include <Servo.h>
 
-/* YOU CAN CUSTOM THESE VARIABLES IF YOU WANT TO ALTER THE TOWER BEHAVIOUR */
+const float MIN_X = 5;
+const float MAX_X = 50;
+const float MIN_Y = 5;
+const float MAX_Y = 35;
 
-// X servo angle will stay in [min_x, max_x] range
-// Y servo angle will stay in [min_y, max_y] range
-// to be ajsuted to the size of your living room
+const float MINIMAL_MOVEMENT = 5;
+const float MIN_MOVE_TIME = 20;
+const float MAX_MOVE_TIME = 50;
 
-float min_x = 5;
-float max_x = 50;
-float min_y = 5;
-float max_y = 35;
-int min_freeze = 200;
-int max_freeze = 3000;
-float minimal_movement = 5;
+// find center of square for starting point
+float c_x = MIN_X + (MAX_X - MIN_X) / 2;
+float c_y = MIN_Y + (MAX_Y - MIN_Y) / 2;
 
-/* YOU SHOULD NOT HAVE TO MODIFY THE CODE BELOW THIS LINE */
+// Instantiating two servos and setting the light pin
+Servo X_SERVO;
+Servo Y_SERVO;
+const int LIGHT_PIN = 13;
+const int SERVO_MOVE_DELAY = 10;
 
-// finding center of square for starting point
-int random_delay;
-float x_position = min_x + (max_x - min_x)/2;
-float y_position = min_y + (max_y - min_y)/2; 
-float x_old_position = x_position;
-float y_old_position = y_position;
-float x_new_position;
-float y_new_position;
-float x_speed;
-float y_speed;
-int movement_time;
-
-// Instantiating two servos
-Servo x_servo;  
-Servo y_servo;
-int pos = 0;
+//
+// SETUP
+//
 
 void setup() {
-  y_servo.attach(6);  // attaches the y servo on pin 6 to the servo object
-  x_servo.attach(9);  // attaches the x servo on pin 9 to the servo object
-  pinMode (13, OUTPUT);
-  digitalWrite (13, HIGH);  // switch on  the laser
-  
-  //Place the servos in the center at the beginning 
-  y_servo.write(y_position); 
-  x_servo.write(x_position);     
+  X_SERVO.attach(9);  // attaches the x servo on pin 9 to the servo object
+  Y_SERVO.attach(6);  // attaches the y servo on pin 6 to the servo object
+  pinMode (LIGHT_PIN, OUTPUT);
+  digitalWrite (LIGHT_PIN, HIGH);  // switch on the laser
+
+  //Place the servos in the center at the beginning
+  X_SERVO.write(c_x);
+  Y_SERVO.write(c_y);
 
 }
+
+// VALIDATOR
+
+const float MIN_DISTANCE = 5;
+void checkMinDistance(float &new_x, float &new_y) {
+  if (abs(new_x - c_x) < MIN_DISTANCE) {
+    if (new_x > c_x) {
+      new_x += MIN_DISTANCE;
+    } else {
+      new_x -= MIN_DISTANCE;
+    }
+  }
+
+  if (abs(new_y - c_y) < MIN_DISTANCE) {
+    if (new_y > c_y) {
+      new_y += MIN_DISTANCE;
+    } else {
+      new_y -= MIN_DISTANCE;
+    }
+  }
+}
+
+//
+// MOVEMENT
+//
+
+void to(float x, float y, float t) {
+  float x_speed = (x - c_x) / t;
+  float y_speed = (y - c_y) / t;
+  for (float pos = 0; pos < t; pos += 1) {
+    c_x += x_speed;
+    c_y += y_speed;
+    X_SERVO.write(c_x);
+    Y_SERVO.write(c_y);
+    delay(SERVO_MOVE_DELAY);
+  }
+  c_x = x; // ensure that the current position is the new position
+  c_y = y;
+}
+
+void curve(float x, float y, float x_control, float y_control, float t) {
+
+}
+
+void toRandom() {
+  float move_time = random(MIN_MOVE_TIME, MAX_MOVE_TIME);
+  float new_x = random(MIN_X + MIN_DISTANCE, MAX_X - MIN_DISTANCE); // this ensures that after we correct for MIN_DISTANCE
+  float new_y = random(MIN_Y + MIN_DISTANCE, MAX_Y - MIN_DISTANCE); // we will still not go over the bounds
+  checkMinDistance(new_x, new_y);
+  to(new_x, new_y, move_time);
+}
+
+void path() {
+
+}
+
+void shake() {
+
+}
+
+void border() {
+  to(MIN_X, MIN_Y, 50);
+  to(MIN_X, MAX_Y, 50);
+  to(MAX_X, MAX_Y, 50);
+  to(MAX_X, MIN_Y, 50);
+}
+
+const int MIN_FLICKS = 8;
+const int MAX_FLICKS = 20;
+const int MIN_FLICK_TIMEOUT = 30;
+const int MAX_FLICK_TIMEOUT = 80;
+void flicker() {
+  float flick_count = random(MIN_FLICKS, 40);
+  bool isOn = true;
+  for (float flicks = 0; flicks < flick_count; flicks += 1) {
+    int flick_timeout = random(MIN_FLICK_TIMEOUT, MAX_FLICK_TIMEOUT);
+    if (isOn) {
+      digitalWrite(LIGHT_PIN, LOW);
+    } else {
+      digitalWrite(LIGHT_PIN, HIGH);
+    }
+    delay(flick_timeout);
+  }
+  digitalWrite(LIGHT_PIN, HIGH);  // ensure the laser is on
+}
+
+const int MIN_VANISH_TIMEOUT = 500;
+const int MAX_VANISH_TIMEOUT = 2000;
+void vanish() {
+  digitalWrite(LIGHT_PIN, LOW);
+  int vanish_timeout = random(MIN_VANISH_TIMEOUT, MAX_VANISH_TIMEOUT);
+  digitalWrite(LIGHT_PIN, HIGH);
+}
+
+//
+// LOOP
+//
 
 void loop() {
-  movement_time = random(10,40);
-  random_delay = random(min_freeze, max_freeze);
-  x_new_position = random(min_x+minimal_movement, max_x-minimal_movement);
-  y_new_position = random(min_y+minimal_movement, max_y-minimal_movement);
-  
-  if( (y_new_position > y_old_position) && (abs(y_new_position - y_old_position) < 5 )) {
-    y_new_position = y_new_position + minimal_movement;
-  }  else if ( (y_new_position < y_old_position) && (abs(y_new_position - y_old_position) < 5 )) {
-    y_new_position = y_new_position - minimal_movement;
-  }
-  
-  if( (x_new_position > x_old_position) && (abs(x_new_position - x_old_position) < 5 )) {
-    x_new_position = x_new_position + minimal_movement;
-  }  else if ( (x_new_position < x_old_position) && (abs(x_new_position - x_old_position) < 5 )) {
-    x_new_position = x_new_position - minimal_movement;
-  }
-  
-  x_speed = (x_new_position - x_old_position)/movement_time;
-  y_speed = (y_new_position - y_old_position)/movement_time;  
-  for (pos = 0; pos < movement_time; pos += 1) { 
-      x_position = x_position + x_speed;
-      y_position = y_position + y_speed;
-      x_servo.write(x_position);  
-      y_servo.write(y_position);                    
-    delay(10); 
-  }
-  x_old_position = x_new_position;
-  y_old_position = y_new_position;
-  delay(random_delay);
-
+  toRandom();
+  delay(500);
 }
-
